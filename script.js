d239f7a012cee80497f27108c204bbbe8a57f7d4
Lucille Trepanier <trepl985@newschool.edu>
2026-01-30 23:02:40 -0500

    Links Practice
    - class demo file

---
+++ script.js
@@ -0,0 +1,4 @@
+// “Find” the `button` via CSS selector, then ”listen” for clicks on it.
+document.querySelector('button').addEventListener('click', () => {
+	alert('The button was clicked!') // Pop an alert!
+})
