6938ae44d05421cae0c84eb92f63de55474d3a3d
Lucille Trepanier <trepl985@newschool.edu>
2026-02-18 13:30:50 -0500

    Added iframe, audio, video elements
    - modal complete and linked on all Build drop down pages

--- build/script.js
+++ build/script.js
@@ -8,13 +8,19 @@ let closeButton = dialog.querySelector('button') // Only looking within `modalDi
 document.querySelector('#content-grid').addEventListener('click', (event) => { // “Listen” for clicks.
     // https://adactio.medium.com/event-target-closest-536c7498a897
     let block = event.target.closest('.content-block') // Find the closest `.content-block` to the click, if there is one.
+    if (!block) return
 
     let title = block.querySelector('.block-title').textContent // grab title FROM that block
     let img = block.querySelector('img')?.src
+    let textContent = block.querySelector('.text-content')?.innerHTML
+    let iframe = block.querySelector('iframe')?.src // video was not working so trying to pull from iframe instead
+    let audio = block.querySelector('audio')?.src // for audio and odd remaining videos
+    let video = block.querySelector('video')?.src // for video
 
     document.getElementById('modal-title').textContent = title || 'Untitled' // textContent https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
-    document.getElementById('modal-image').innerHTML = img ? `<img src="${img}" class="modal-img">` : '' // ? = "if true use this", :  = "otherwise use this". '' = empty string (nothing)
-    
+    document.getElementById('modal-image').innerHTML = img  ? `<img src="${img}" class="modal-img">` : textContent ? `<div class="modal-text">${textContent}</div>` : iframe ? `<iframe src="${iframe}" style="width:100%; height:300px" frameborder="0" allowfullscreen></iframe>` : audio ? `<audio controls src="${audio}" style="width:100%"></audio>` : video ? `<video controls src="${video}" style="width:100%"></video>` : '' // ? = "if true use this", :  = "otherwise use this". '' = empty string (nothing)
+    // if image exists, show image, if no image but text exists, show text, if no image or text but iframe exists, show iframe, if no image or text or iframe exists, show audio, ... show video etc. if none of the above exist, show nothing,
+
     dialog.showModal() // This opens it up.
 })
 
